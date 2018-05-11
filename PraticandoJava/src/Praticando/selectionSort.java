3c38a6dc713bd8f9bd535758fbcc1a94ced02838
fernando <eaofrnnd@gmail.com>
2018-05-10 21:40:43 -0300

    primeiro commit, selectionSort

---
+++ PraticandoJava/src/Praticando/selectionSort.java
@@ -0,0 +1,29 @@
+package Praticando;
+
+import java.util.Arrays;
+
+public class selectionSort {
+	
+	public static void sort(String[] a) {
+		for (int i = 0; i < a.length - 1; i++) {
+			for (int j = i + 1; j < a.length; j++) {
+				if (a[i].compareToIgnoreCase(a[j]) > 0) {
+					String temp = a[j];
+					a[j] = a[i];
+					a[i] = temp;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		String[] nome = {"Fernandodes", "Fernando", "Rodrigo", "Alfredo", "Marlom", "aaa", "A", "a", "z", "bb", "aa", "asd", "c", "bcd"};
+		
+		System.out.println(Arrays.toString(nome));
+		sort(nome);
+		System.out.println(Arrays.toString(nome));	
+		
+
+	}
+}
